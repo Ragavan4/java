64392330a280f59a5dee88ca8296040575614902
Ragavan4 <111166171+Ragavan4@users.noreply.github.com>
2022-11-17 11:35:07 +0530

    Add files via upload

---
+++ lab/Stringdemo.java
@@ -0,0 +1,10 @@
+public class Stringdemo{    
+public static void main(String args[]){    
+String s1="ragava";    
+char ch[]={'v','a','s','u','k','i'};    
+String s2=new String(ch);   
+String s3=new String("dhanush");   
+System.out.println(s1);    
+System.out.println(s2);    
+System.out.println(s3);    
+}}    

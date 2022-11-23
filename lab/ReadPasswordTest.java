1c408b7af2013a0a3c46a7e884a804dad358cea6
Ragavan4 <111166171+Ragavan4@users.noreply.github.com>
2022-11-23 10:14:12 +0530

    Create ReadPasswordTest.java

---
+++ lab/ReadPasswordTest.java
@@ -0,0 +1,12 @@
+import java.io.*;
+class ReadPasswordTest
+{
+	public static void main(String[]args)
+	{
+	Console c = System.console();
+	System.out.println("Enter password:");
+	char[]ch = c.readPassword();
+	String pass = String.valueOf(ch);
+	System.out.println("Password is:"+pass);
+}
+}
